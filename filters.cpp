6300dd6da86b1e8028955c8c7963a1c348d973a2
maky189 <maky188pgt555@gmail.com>
2025-03-26 14:08:54 -0100

    added blur and edge filters

--- filters.cpp
+++ filters.cpp
@@ -11,17 +11,17 @@ void sepia(rgbt** image, int width, int height) {
     {
         for(int j = 0; j < width; ++j)
         {
-           float Red = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
-           float Green = round(0.349 * image[i][j].rgbtRed + 0.689 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
-           float Blue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
+           int originalRed = image[i][j].rgbtRed;
+           int originalGreen = image[i][j].rgbtGreen;
+           int originalBlue = image[i][j].rgbtBlue;
 
-           if(Red > 255) Red = 255;
-           if(Green > 255) Green = 255;
-           if(Blue > 255) Blue = 255;
+           int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+           int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+           int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
 
-           image[i][j].rgbtRed = (int)Red;
-           image[i][j].rgbtGreen = (int)Green;
-           image[i][j].rgbtBlue = (int)Blue;
+           image[i][j].rgbtRed = (sepiaRed > 255) ? 255 : sepiaRed;
+           image[i][j].rgbtGreen = (sepiaGreen > 255) ? 255 : sepiaGreen;
+           image[i][j].rgbtBlue = (sepiaBlue > 255) ? 255 : sepiaBlue;
         }
     }
 }
@@ -32,7 +32,7 @@ void grayscale(rgbt** image, int width, int height) {
     {
         for(int j = 0; j < width; ++j)
         {
-            int average = (int)round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+            int average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
 
             image[i][j].rgbtBlue = average;
             image[i][j].rgbtGreen = average;
@@ -56,9 +56,95 @@ void reflect(rgbt** image, int width, int height)
 }
 
 void blur(rgbt** image, int width, int height) {
-    // TODO: Implement blur filter
+    // Dynamically allocate memory for the copy
+    rgbt** copy = new rgbt*[height];
+    for (int i = 0; i < height; ++i) {
+        copy[i] = new rgbt[width];
+        for (int j = 0; j < width; ++j) {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            float Red = 0, Green = 0, Blue = 0;
+            int counter = 0;
+
+            for (int li = -1; li <= 1; ++li) {
+                for (int lj = -1; lj <= 1; ++lj) {
+                    int ni = i + li;
+                    int nj = j + lj;
+
+                    if (ni >= 0 && ni < height && nj >= 0 && nj < width) {
+                        Red += copy[ni][nj].rgbtRed;
+                        Green += copy[ni][nj].rgbtGreen;
+                        Blue += copy[ni][nj].rgbtBlue;
+                        counter++;
+                    }
+                }
+            }
+
+            image[i][j].rgbtRed = round(Red / counter);
+            image[i][j].rgbtGreen = round(Green / counter);
+            image[i][j].rgbtBlue = round(Blue / counter);
+        }
+    }
+
+    // Free dynamically allocated memory
+    for (int i = 0; i < height; ++i) {
+        delete[] copy[i];
+    }
+    delete[] copy;
 }
 
 void edge(rgbt** image, int width, int height) {
-    // TODO: Implement edge detection filter
+    int gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+    // Dynamically allocate memory for the copy
+    rgbt** copy = new rgbt*[height];
+    for (int i = 0; i < height; ++i) {
+        copy[i] = new rgbt[width];
+        for (int j = 0; j < width; ++j) {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            int redX = 0, greenX = 0, blueX = 0;
+            int redY = 0, greenY = 0, blueY = 0;
+
+            for (int li = -1; li <= 1; ++li) {
+                for (int lj = -1; lj <= 1; ++lj) {
+                    int ni = i + li;
+                    int nj = j + lj;
+
+                    if (ni >= 0 && ni < height && nj >= 0 && nj < width) {
+                        redX += copy[ni][nj].rgbtRed * gx[li + 1][lj + 1];
+                        greenX += copy[ni][nj].rgbtGreen * gx[li + 1][lj + 1];
+                        blueX += copy[ni][nj].rgbtBlue * gx[li + 1][lj + 1];
+
+                        redY += copy[ni][nj].rgbtRed * gy[li + 1][lj + 1];
+                        greenY += copy[ni][nj].rgbtGreen * gy[li + 1][lj + 1];
+                        blueY += copy[ni][nj].rgbtBlue * gy[li + 1][lj + 1];
+                    }
+                }
+            }
+
+            int red = round(sqrt(redX * redX + redY * redY));
+            int green = round(sqrt(greenX * greenX + greenY * greenY));
+            int blue = round(sqrt(blueX * blueX + blueY * blueY));
+
+            image[i][j].rgbtRed = (red > 255) ? 255 : red;
+            image[i][j].rgbtGreen = (green > 255) ? 255 : green;
+            image[i][j].rgbtBlue = (blue > 255) ? 255 : blue;
+        }
+    }
+
+    // Free dynamically allocated memory
+    for (int i = 0; i < height; ++i) {
+        delete[] copy[i];
+    }
+    delete[] copy;
 }
